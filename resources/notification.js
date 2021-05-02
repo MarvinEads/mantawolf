fc270739a886aaec9ce12b5261996416e2616820
Marvin Eads <marvin.eads@eadshome.net>
2021-05-02 17:41:22 -0500

    simple fixes

--- resources/notification.js
+++ resources/notification.js
@@ -1,6 +1,6 @@
 function postContactForm(){
     var xmlhttp = new XMLHttpRequest();
-    var url = "https://getsimpleform.com/messages/ajax?form_api_token=2cf6bd15735e916486a2660d1143d342";
+    var url = "https://getsimpleform.com/messages/ajax?form_api_token=${{ secrets.SIMPLE_FORM }}";
     var data = new FormData();
 
     url += "&name=" + document.getElementById("name").value;
@@ -11,32 +11,6 @@ function postContactForm(){
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             toggleNotification('success', 'Contact Form Successfully Posted', 'contact');
-        }
-    }
-
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send(data);
-
-    console.log(xmlhttp);
-}
-
-function postSignupForm(){
-    var xmlhttp = new XMLHttpRequest();
-    var url = "https://getsimpleform.com/messages/ajax?form_api_token=2cf6bd15735e916486a2660d1143d342";
-    var data = new FormData();
-
-    url += "&title=WRFA Signup";
-    var msg = "name = " + document.getElementById("name").value + "<br />";
-    msg += "email = " + document.getElementById("email").value + "<br />";
-    msg += "birthdate = " + document.getElementById("birthdate").value + "<br />";
-    msg += "address = " + document.getElementById("address").value + "<br />";
-    msg += "phone = " + document.getElementById("phone").value + "<br />";
-
-    url += "&message=" + msg;
-
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            toggleNotification('success', 'Signup successful.  You should receive information shortly.', 'signup');
         }
     }
 
